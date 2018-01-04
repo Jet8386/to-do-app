5e1111558b8cd185e3fc8babf77abea6a9d70fb3
Jenna Thompson <31006550+Jet8386@users.noreply.github.com>
2018-01-04 13:17:22 -0500

    Select DOM Elements

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+function onReady() {
+	const addToDoForm = document.getElementById('addToDoForm');
+	const newToDoText = document.getElementById('newToDoText');
+	const toDoList = document.getElementById('toDoList'); 
+
+}
+
 window.onload = function(){
 	alert("The window has loaded!")
+	onReady();
 };
