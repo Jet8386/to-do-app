b455903fafc5fd86f3a1c01ccba81fe4a14c2e2b
Jenna Thompson <31006550+Jet8386@users.noreply.github.com>
2018-01-04 13:41:34 -0500

    Completing the Event Handler

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ function onReady() {
 	const newToDoText = document.getElementById('newToDoText');
 	const toDoList = document.getElementById('toDoList'); 
 
+	addToDoForm.addEventListener('submit', event => {
+	event.preventDefault();
+
+	//get the text
+	let title = newToDoText.value;
+
+	//create a new li
+	let newLi = document.createElement('li');
+	
+	//create new input
+	let checkbox = document.createElement('input');
+
+	//set the input's type to checkbox
+	checkbox.type = "checkbox";
+
+	//set the title
+	newLi.appendChild(checkbox);
+
+	//attach the checkbox to the li
+	newLi.appendChild(checkbox);
+
+	//attach the li to the ul 
+	toDoList.appendChild(newLi);
+
+	//empty the input
+	newToDoText.value= '';
+
+	});
 }
 
 window.onload = function(){
