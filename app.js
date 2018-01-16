3b9c27579823b95f35689d454edb6d60f1380640
Jenna Thompson <31006550+Jet8386@users.noreply.github.com>
2018-01-16 11:14:25 -0500

    started exercise

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 function onReady() {
 
+	let id = 0;
+
+
 	let toDos = [];
 	
 	const addToDoForm = document.getElementById('addToDoForm');
@@ -13,7 +16,8 @@ function onReady() {
 
                 toDos.push({
                         title:newToDoText.value,
-                        complete:false
+                        complete:false,
+			id:0
                 });
 
 		newToDoText.value = '';
